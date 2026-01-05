e05914207076fc25cbadace8086fa844968e0d53
Steven Clontz <steven.clontz@gmail.com>
2026-01-05 03:01:30 +0000

    add proof of concept prefigure builder

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import re
 import shutil
 from tempfile import TemporaryDirectory
 from os import environ
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, make_response
+import prefig
 from pretext.project import Project
 from pretext.logger import get_log_error_flush_handler
 
@@ -36,10 +37,14 @@ def icon_svg():
 
 
 @app.route("/", methods=["GET", "POST"])
-def api():
+def pretext():
     if request.method == "GET":
         if environ.get("DEVELOPMENT") == "true":
-            return render_template("api.html", token=TOKEN)
+            title = r"Hello world! Goodbye <m>\LaTeX</m>!"
+            source = """
+<p>This is math: <m>x^2</m>.</p>
+            """
+            return render_template("api.html", token=TOKEN, source=source, title=title)
         return "PreTeXt.Plus Build API"
     if request.form.get('token') != TOKEN:
         return "Invalid token", 401
@@ -74,3 +79,33 @@ def api():
             return response, 500
         # return the generated HTML file
         return (temp_dir / "output" / "article.html").read_text()
+
+
+@app.route("/prefigure/", methods=["GET", "POST"])
+def prefigure():
+    if request.method == "GET":
+        source = """
+<diagram dimensions="(300,300)" margins="5">
+  <definition> f(x) = exp(x/3)*cos(x) </definition>
+  <definition> a = 1 </definition>
+  <coordinates bbox="(-4,-4,4,4)">
+    <grid-axes xlabel="x" ylabel="y"/>    
+    <graph function="f"/>
+    <tangent-line function="f" point="a"/>
+    <point p="(a,f(a))">
+      <m>(a,f(a))</m>
+    </point>
+  </coordinates>
+</diagram>
+        """
+        return render_template("api.html", token=TOKEN, source=source, title=None)
+        # if environ.get("DEVELOPMENT") == "true":
+        #     return render_template("api.html", token=TOKEN)
+        # return "PreTeXt.Plus Prefigure Build API"
+    if request.form.get('token') != TOKEN:
+        return "Invalid token", 401
+    source = request.form.get('source')
+    svg = prefig.engine.build_from_string('svg', source, environment="pretext")
+    response =  make_response(svg)
+    response.headers['Content-type'] = 'image/svg+xml'
+    return response
